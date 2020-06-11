d64b1eee7168025200d6cc48be40e067f6a6f6d4
Israel-Laguan <israellaguan@gmail.com>
2020-06-11 08:53:11 -0500

    fix(COMMENTS): Erase extra lines in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-// const express = require('express')
 const rateLimit = require('express-rate-limit')
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 15 minutes
@@ -12,10 +11,6 @@ const system = require('./system')
 module.exports = (server) => {
   try {
     server.use('/', system)
-    // server.get('/', function (_, res) {
-    //   res.sendFile('/index.html', { root: path.join(__dirname, '../webpage') });
-    // });
-    // server.use(express.static(path.join(__dirname, '../webpage')));
     server.use('/api', versionController)
     server.use('/api', limiter)
   } catch (error) {
